31e0345ec56042387f53cb154692a01ef3963892
Myo <myothuwinaung1991@gmail.com>
2020-04-26 13:21:29 -0400

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express ();
 //  single app
 
 app.get ('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 })
 
 const PORT = process.env.PORT || 5000;
